dddab45a34e425e0c8bb0aea30b62b9207c3c7b1
JohnnyJ0622 <johnnyj0622@yahoo.com>
2016-11-11 23:25:02 +1300

    2.2.1 Creating discoverable beans

---
+++ src/main/java/com/springinaction/soundsystem/SgtPeppers.java
@@ -0,0 +1,18 @@
+package com.springinaction.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Listing 2.2 @CompactDisc-annotated SgtPeppers implements CompactDisc
+ * Created by Johnny on 11/11/2016.
+ */
+@Component
+public class SgtPeppers implements CompactDisc {
+
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    public void play() {
+        System.out.println("Playing  " + title + " by" + artist);
+    }
+}
