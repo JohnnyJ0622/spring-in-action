c7f13456f6c3b68aca4bf734e55bb056aa25c563
JohnnyJ0622 <johnnyj0622@yahoo.com>
2016-11-23 23:32:37 +1300

    Handling parameters in advice

---
+++ src/main/java/com/springinaction/soundsystem/config/TrackCounterConfig.java
@@ -0,0 +1,42 @@
+package com.springinaction.soundsystem.config;
+
+import com.springinaction.soundsystem.BlankDisc;
+import com.springinaction.soundsystem.CompactDisc;
+import com.springinaction.soundsystem.TrackCounter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Johnny on 11/23/2016.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class TrackCounterConfig {
+    @Bean
+    public CompactDisc sgtPeppers() {
+        BlankDisc cd = new BlankDisc();
+        cd.setTitle("Sgt. Pepper's Lonely Hearts Club Band");
+        cd.setArtist("The Beatles");
+        List<String> tracks = new ArrayList<String>();
+        tracks.add("Sgt. Pepper's Lonely Hearts Club Band");
+        tracks.add("With a Little Help from My Friends");
+        tracks.add("Lucky in the Sky with Diamonds");
+        tracks.add("Getting Better");
+        tracks.add("Fixing a Hole");
+        tracks.add("She's Leaving Home");
+        tracks.add("Being for the Benefit of Mr. Kite!");
+        tracks.add(	"Within You Without You");
+        cd.setTracks(tracks);
+        return cd;
+    }
+
+    @Bean
+    public TrackCounter trackCounter() {
+        return new TrackCounter();
+    }
+
+}
